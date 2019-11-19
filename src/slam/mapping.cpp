f147c17e04f9e9ec04ee86abd379fd24f6170a49
dany-d <danyd4233@gmail.com>
2019-11-18 19:15:34 -0500

    adding actionmodelv1

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -19,6 +19,11 @@ int metersToCellX(float x, OccupancyGrid& map)
     return std::floor(x * map.cellsPerMeter()) + (map.widthInCells() / 2);
 }
 
+int metersToCellY(float x, OccupancyGrid& map)
+{
+    return std::floor(x * map.cellsPerMeter()) + (map.heightInCells() / 2);
+}
+
 void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGrid& map)
 {
     //////////////// TODO: Implement your occupancy grid algorithm here ///////////////////////
@@ -31,27 +36,29 @@ void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGr
     started_ = true;
     return;
   }
-
+  // last_pose_ = pose;
   // Moving laser scan to interpolate current robot pose
   MovingLaserScan ml_scan(scan, last_pose_, pose, rayStride_);
-
+  int new_logOdd = 0;
 	// This algorith is supposed to update the log odds associated with each cell //
 
 	// Need to determine the cells through which the laser passes through for each ray of laser
 	// Creating for loop to iterate through each laser array
 	for (unsigned int i=0; i<ml_scan.size(); i++){
     adjusted_ray_t ad_ray = ml_scan.at(i);
-    	// Current robot pose in world frame
-    	int x0 = metersToCellX(ad_ray.origin.x, map);
-    	int y0 = metersToCellX(ad_ray.origin.y, map);
+  	// Current robot pose in world frame
+  	int x0 = metersToCellX(ad_ray.origin.x, map);
+  	int y0 = metersToCellY(ad_ray.origin.y, map);
 
-    	float range = ad_ray.range;
-    	float theta = ad_ray.theta; // This is the global frame theta!
-    	cout<<i<<" "<<x0<<" "<<y0<<" "<<range<<" "<<theta<<endl;
-
+  	float range = ad_ray.range;
+  	float theta = ad_ray.theta; // This is the global frame theta!
+  	//cout<<i<<" "<<x0<<" "<<y0<<" "<<range<<" "<<theta<<endl;
+    if(range > kMaxLaserDistance_){
+         continue;
+      }
 		// End of laser scan in world frame
-    	int x1 = metersToCellX(ad_ray.origin.x + range*cos(theta), map);
-   		int y1 = metersToCellX(ad_ray.origin.y + range*sin(theta), map);
+    int x1 = metersToCellX(ad_ray.origin.x + range*cos(theta), map);
+   	int y1 = metersToCellY(ad_ray.origin.y + range*sin(theta), map);
 		int dx = abs(x1-x0);
 		int dy = abs(y1-y0);
 		int sx = (x0<x1)? 1 : -1;
@@ -59,64 +66,32 @@ void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGr
 		int err = dx-dy;
 		int x = x0;
 		int y = y0;
-    	//TODO: inifite loop here.
 		while(x != x1 || y != y1){
 
 		  // Updating the odds for each cell through which laser array passes
-			CellOdds new_logOdd;
-      //TODO: need to transform between global (meter) to occupancy grid (int) frame
-      //TODO: clamp logOdds
 			new_logOdd = map.logOdds(x,y)  - kMissOdds_;
       new_logOdd = new_logOdd < -127 ? -127: new_logOdd;
 			map.setLogOdds(x,y, new_logOdd);
-      cout<<"Miss: "<<x<<" "<<y<<" "<< int(new_logOdd)<<endl;
+
 			// Computing the next x,y cells through which laser passes using Breshenham's Algorithm
-		    float e2 = 2*err;
-		    if (e2 >= -dy)
-			    {
-					err -= dy;
-					x += sx;
-				}
+	    int e2 = 2*err;
+	    if (e2 >= -dy)
+		    {
+				err -= dy;
+				x += sx;
+			}
 
-		    if (e2 <= dx)
-			    {
-			    	err += dx;
-					y += sy;
-				}
+	    if (e2 <= dx)
+		    {
+		    	err += dx;
+				y += sy;
+			}
 	 	}
-	 	CellOdds new_logOdd = map.logOdds(x1,y1) +  kHitOdds_;
+	 	new_logOdd = map.logOdds(x1,y1) +  kHitOdds_;
 	 	new_logOdd = new_logOdd > 127 ? 127: new_logOdd;
-    	map.setLogOdds(x1,y1, new_logOdd);
-
+    map.setLogOdds(x1,y1, new_logOdd);
+    //cout<<"Hit: "<<x<<" "<<y<<" "<< int(new_logOdd)<<endl;
 	}
+  //cout<< int(map.logOdds(124,123)) <<endl;
   last_pose_ = pose;
 }
-
-// CellOdds inverse_sensor_model(adjusted_ray_t ad_ray, int x, int y, OccupancyGrid& map, float l0){
-
-// 	float x_i = cellToMetersX(x, map);
-// 	float y_i = cellToMetersX(y, map);
-// 	float x_0 = ad_ray.origin.x;
-// 	float y_0 = ad_ray.origin.y;
-
-// 	float r = sqrt((x_i - x_0) * (x_i - x_0) + (y_i - y_0) * (y_i - y_0));
-// 	float phi = atan2(y_i - y_0, x_i - x_0) - ad_ray.theta;
-// 	float zkt = ad_ray.range;
-// 	float alpha = sqrt(2) * map.metersPerCell();
-// 	float beta = atan2(r, alpha / 2);
-// 	float kMaxLaserDistance_=10.0;
-// 	int8_t kHitOdds_=3;
-// 	int8_t kMissOdds_=1;
-
-// 	// kMaxLaserDistance_ is private member of Mapping class
-// 	if (r > std::min(kMaxLaserDistance_, zkt + alpha/2) || abs(phi)>(beta/2)){
-// 		return l0;
-// 	}
-
-// 	if (zkt < kMaxLaserDistance_ && abs(r - zkt)<alpha/2){
-// 		return kHitOdds_;
-// 	}
-
-// 	if (r<=zkt)
-// 		return -kMissOdds_;
-// }
